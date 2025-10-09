a0276325fccaee95a8a778ba23bc80f25a51f05f
dong881 <minghunghsu.taiwan@gmail.com>
2025-10-09 15:25:29 +0800

    Specify full path for asn1c command in justTest.sh

--- RRC/justTest.sh
+++ RRC/justTest.sh
@@ -4,7 +4,7 @@ cp Src/test_RRC.c .
 cp Src/testRRC.mk .
 cp Src/rrc_15_3.asn1 .
 
-asn1c -fcompound-names -fno-include-deps -findirect-choice -gen-PER -no-gen-OER rrc_15_3.asn1
+/opt/asn1c/bin/asn1c -fcompound-names -fno-include-deps -findirect-choice -gen-PER -no-gen-OER rrc_15_3.asn1
 
 cp Src/SearchSpace.c.new SearchSpace.c
 
